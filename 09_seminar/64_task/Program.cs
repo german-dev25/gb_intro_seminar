c235a49f7fba1ad804f655f2f40a3a048c063290
german25 <german.apx@yandex.ru>
2023-04-10 22:49:15 +0300

    Make homework #9

---
+++ 09_seminar/64_task/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void PrintNumbers(int n)
+{
+    if (n < 1)
+    {
+        return;
+    }
+    
+    Console.Write("{0}, ", n);
+    PrintNumbers(n - 1);
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+PrintNumbers(n);
---
+++ 09_seminar/66_task/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+int sum = SumFromMToN(m, n);
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна {sum}");
+
+int SumFromMToN(int m, int n)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    else if (m > n)
+    {
+        return 0;
+    }
+    else
+    {
+        return m + SumFromMToN(m + 1, n);
+    }
+}
